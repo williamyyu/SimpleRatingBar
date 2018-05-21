ad7ad93f40d76950a86197b54056b155ac8b23a2
Adrien Vitti <adrien@smartnsoft.com>
2018-05-21 18:53:47 +0200

    Fixed rating preview for the scaleRatingBar

--- library/src/main/java/com/willy/ratingbar/ScaleRatingBar.java
+++ library/src/main/java/com/willy/ratingbar/ScaleRatingBar.java
@@ -62,8 +62,22 @@ public class ScaleRatingBar extends AnimationRatingBar {
                 continue;
             }
 
+          if (isInEditMode())
+          {
+            if (ratingViewId == maxIntOfRating)
+            {
+              partialView.setPartialFilled(rating);
+            }
+            else
+            {
+              partialView.setFilled();
+            }
+          }
+          else
+          {
             mRunnable = getAnimationRunnable(rating, partialView, ratingViewId, maxIntOfRating);
             postRunnable(mRunnable, ANIMATION_DELAY);
+          }
         }
     }
 
